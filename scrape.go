d3f6e7838c04036f9d09022a7f9c2ed1d5d2ec14
Parul Bhardwaj <111934039+ParulK-bhardwaj@users.noreply.github.com>
2024-04-15 19:20:08 -0400

    serialize the struct to JSON and stdout

--- scrape.go
+++ scrape.go
@@ -1,44 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/gocolly/colly"
 )
 
-// struct Restuarant holds the data about a restaurant
 type Restaurant struct {
-	Name    string
-	Cuisine string
-	Rating  string // Michelin stars or Bib Gourmand
+	Name    string `json:"name"`
+	Cuisine string `json:"cuisine"`
+	Rating  string `json:"rating"`
 }
 
-// main() contains code adapted from example found in Colly's docs:
-// http://go-colly.org/docs/examples/basic/
+type RestaurantList struct {
+	Restaurants []Restaurant `json:"restaurants"`
+}
+
 func main() {
-	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// General selector for all the restuarants listed on the page
+	var restaurants RestaurantList
+
 	restaurantContainerSelector := "section.section-main.search-results.search-listing-result div.restaurant__list-row.js-restaurant__list_items > div"
 
-	// OnHTML callback
 	c.OnHTML(restaurantContainerSelector, func(e *colly.HTMLElement) {
-		// Extract restaurant data
 		restaurant := Restaurant{
-			Name:    e.ChildText("div > div.flex-fill > div > div:nth-child(1) > div:nth-child(2) > h3"),
-			Cuisine: e.ChildText("div > div.flex-fill > div > div.row.flex-fill > div > div.align-items-end.js-match-height-bottom > div:nth-child(2)"),
+			Name:    cleanText(e.ChildText("div > div.flex-fill > div > div:nth-child(1) > div:nth-child(2) > h3")),
+			Cuisine: cleanText(e.ChildText("div > div.flex-fill > div > div.row.flex-fill > div > div.align-items-end.js-match-height-bottom > div:nth-child(2)")),
 			Rating:  extractRating(e.ChildAttrs("div > div.flex-fill > div > div:nth-child(1) > div:nth-child(1) > div > span > img", "src")),
 		}
 
 		if restaurant.Name != "" {
-			// Print restuarant list with information
-			fmt.Printf("Found restaurant: %s, Price range and Cuisine: %s, Rating: %s\n", restaurant.Name, restaurant.Cuisine, restaurant.Rating)
+			restaurants.Restaurants = append(restaurants.Restaurants, restaurant)
 		}
 	})
 
-	// Before making a request print "Visiting ..."
+	c.OnScraped(func(r *colly.Response) {
+		jsonData, err := json.Marshal(restaurants)
+		if err != nil {
+			fmt.Println("Error serializing JSON:", err)
+			return
+		}
+		fmt.Println(string(jsonData))
+		fmt.Println("Finished", r.Request.URL)
+	})
+
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
@@ -47,22 +56,16 @@ func main() {
 		fmt.Println("Something went wrong:", err)
 	})
 
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println("Finished", r.Request.URL)
-	})
-
-	// Start scraping on the Micheline guide toronto page
 	c.Visit("https://guide.michelin.com/ca/en/restaurants/1-star-michelin/2-stars-michelin/bib-gourmand?q=toronto%2C+Ontario%2C+Canada")
 }
 
-// extractRating finds the rating based on the img src
 func extractRating(imageSrcs []string) string {
 	starCount := 0
 	for _, src := range imageSrcs {
 		if strings.Contains(src, "1star") {
 			starCount++
 		} else if strings.Contains(src, "bib-gourmand") {
-			return "Bib Gourmand "
+			return "Bib Gourmand"
 		}
 	}
 	switch starCount {
@@ -70,7 +73,18 @@ func extractRating(imageSrcs []string) string {
 		return "1 star"
 	case 2:
 		return "2 stars"
+	case 3:
+		return "3 stars"
 	default:
 		return "No Rating"
 	}
+}
+
+func cleanText(input string) string {
+	cleaned := strings.ReplaceAll(input, "\n", " ")
+	cleaned = strings.ReplaceAll(cleaned, "\t", " ")
+	// reg exp to replace multiple spaces with no space
+	re := regexp.MustCompile(`\s+`)
+	cleaned = re.ReplaceAllString(cleaned, "")
+	return strings.TrimSpace(cleaned)
 }
